d60d405dd4d8d29fbf5bbd881b6dc264c380ba6a
inversemaha <mahajur88@gmail.com>
2021-05-18 23:03:23 +0600

    done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact-us', [ContactController::class, 'getContact']);
+Route::post('contact-us', [ContactController::class,'saveContact'])->name('contact-form.store');
+
